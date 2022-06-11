86602a5826b4ab12523a43202f4749d79157ed5d
Zihao <zihao-lee@outlook.com>
2022-06-11 13:26:58 +0800

    删除问卷(未测试)

--- src/main/java/com/aim/questionnaire/controller/QuestionnaireController.java
+++ src/main/java/com/aim/questionnaire/controller/QuestionnaireController.java
@@ -27,50 +27,84 @@ public class QuestionnaireController {
     @Autowired
     private QuestionnaireService questionnaireService;
 
-
     /**
      * 查询全部问卷的信息
+     * 
      * @param questionnaireEntity
      * @return
      */
-    @RequestMapping(value = "/queryQuestionnaireList",method = RequestMethod.POST, headers = "Accept=application/json")
-    public HttpResponseEntity queryQuestionnaireList(@RequestBody(required = false) QuestionnaireEntity questionnaireEntity) {
+    @RequestMapping(value = "/queryQuestionnaireList", method = RequestMethod.POST, headers = "Accept=application/json")
+    public HttpResponseEntity queryQuestionnaireList(
+            @RequestBody(required = false) QuestionnaireEntity questionnaireEntity) {
         HttpResponseEntity httpResponseEntity = new HttpResponseEntity();
         return httpResponseEntity;
     }
 
     /**
      * 根据id删除问卷
+     * 
      * @param questionnaireEntity
      * @return
      */
-    @RequestMapping(value = "/deleteQuestionnaireById",method = RequestMethod.POST, headers = "Accept=application/json")
+    @RequestMapping(value = "/deleteQuestionnaireById", method = RequestMethod.POST, headers = "Accept=application/json")
     public HttpResponseEntity deleteQuestionnaireById(@RequestBody QuestionnaireEntity questionnaireEntity) {
         HttpResponseEntity httpResponseEntity = new HttpResponseEntity();
+        try {
+            int result = questionnaireService.deleteQuestionnaireById(questionnaireEntity);
+            if (result == -1) {
+                httpResponseEntity.setCode(Constans.EXIST_CODE);
+                httpResponseEntity.setMessage(Constans.PROJECT_EXIST_MESSAGE);
+            } else {
+                httpResponseEntity.setCode(Constans.SUCCESS_CODE);
+                httpResponseEntity.setMessage(Constans.DELETE_MESSAGE);
+            }
+        } catch (Exception e) {
+            logger.info("deleteQuestionnaireById 删除问卷>>>>>>>>>>>" + e.getLocalizedMessage());
+            httpResponseEntity.setCode(Constans.EXIST_CODE);
+            httpResponseEntity.setMessage(Constans.EXIST_MESSAGE);
+        }
         return httpResponseEntity;
     }
 
     /**
      * 添加问卷
+     * 
      * @param questionnaireEntity
      * @return
      */
-    @RequestMapping(value = "/addQuestionnaireInfo",method = RequestMethod.POST, headers = "Accept=application/json")
+    @RequestMapping(value = "/addQuestionnaireInfo", method = RequestMethod.POST, headers = "Accept=application/json")
     public HttpResponseEntity addQuestionnaireInfo(@RequestBody QuestionnaireEntity questionnaireEntity) {
         HttpResponseEntity httpResponseEntity = new HttpResponseEntity();
-        
+        // try {
+        // int result = questionnaireService.addQuestionnaireInfo(questionnaireEntity,
+        // questionnaireEntity.getCreatedBy());
+        // if(result == 3) {
+        // httpResponseEntity.setCode(Constans.EXIST_CODE);
+        // httpResponseEntity.setMessage(Constans.EXIST_MESSAGE);
+        // }else {
+        // httpResponseEntity.setCode(Constans.SUCCESS_CODE);
+        // httpResponseEntity.setMessage(Constans.ADD_MESSAGE);
+        // }
+        // } catch (Exception e) {
+        // logger.info("addQuestionnaireInfo 创建问卷的基本信息>>>>>>>>>>>" +
+        // e.getLocalizedMessage());
+        // httpResponseEntity.setCode(Constans.EXIST_CODE);
+        // httpResponseEntity.setMessage(Constans.EXIST_MESSAGE);
+        // }
+
         return httpResponseEntity;
     }
 
     /**
      * 根据问卷id修改问卷
+     * 
      * @param questionnaireEntity
      * @return
      */
-    @RequestMapping(value = "/modifyQuestionnaireInfo",method = RequestMethod.POST, headers = "Accept=application/json")
+    @RequestMapping(value = "/modifyQuestionnaireInfo", method = RequestMethod.POST, headers = "Accept=application/json")
     public HttpResponseEntity modifyQuestionnaireInfo(@RequestBody QuestionnaireEntity questionnaireEntity) {
         HttpResponseEntity httpResponseEntity = new HttpResponseEntity();
-        
+
         return httpResponseEntity;
     }
 
